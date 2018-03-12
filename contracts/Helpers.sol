e2f96047b73a5e53acde2eab355713275179eb68
Aditya Jha <aditya.jha@indifi.com>
2018-03-12 11:48:25 +0530

    added helpers contract

---
+++ contracts/Helpers.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.20;
+
+
+contract Helpers {
+
+
+    function _stringToBytes32(string s) internal pure returns (bytes32 result) {
+        if (bytes(s).length == 0) {
+            return 0x0;
+        }
+        assembly {
+            result := mload(add(s, 32))
+        }
+    }
+
+    function isContract(address _base) internal view returns (bool _r) {
+        assembly {
+            _r := gt(extcodesize(_base), 0)
+        }
+    }
+}
