3328237f7d1bf45ddf06b65e95584a225cb473b1
Aditya Jha <aditya.jha@indifi.com>
2018-03-12 12:16:47 +0530

    added safe math functions to helpers

--- contracts/Helpers.sol
+++ contracts/Helpers.sol
@@ -18,4 +18,24 @@ contract Helpers {
             _r := gt(extcodesize(_base), 0)
         }
     }
+
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
 }
