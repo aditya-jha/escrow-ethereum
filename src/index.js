7e5e03b0fffb3e3893bf0c0a56da3f932d344226
Aditya Jha <aditya.jha@indifi.com>
2018-03-12 12:15:27 +0530

    copied config from ethereum-wall

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+ * created by aditya on 12/03/18
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <h1>Hello World</h1>,
+    document.getElementById('root')
+);
