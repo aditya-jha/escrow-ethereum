0c198408d35bc4c6263ee7b7e6864e61dde41c38
Aditya Jha <aditya.jha@indifi.com>
2018-03-17 20:26:17 +0530

    working on transactions page

--- src/containers/Transactions/index.js
+++ src/containers/Transactions/index.js
@@ -18,7 +18,7 @@ class Transactions extends React.Component {
             <div>
                 <Header {...this.props}/>
                 <div className="page_margin row">
-                    <div className="col">
+                    <div className="col-12">
                         <div className="card my-3 ml-3 mt-3 mr-3">
                             <div className="card-body">
                                 <h5 className="card-title text-center">Add Transaction</h5>
@@ -34,6 +34,30 @@ class Transactions extends React.Component {
                                     </div>
                                 </div>
                                 <button className="btn btn-primary" onClick={this.addTransaction.bind(this)}>Add Transaction</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <div className="card my-3 ml-3 mt-3 mr-3">
+                            <div className="card-body">
+                                <h5 className="card-title text-center">Transactions</h5>
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th>S. No.</th>
+                                            <th>Transaction</th>
+                                            <th>Current Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {transactions.transactions.map((t, index) => (
+                                            <tr key={t.txHash}>
+                                                <td>{index + 1}</td>
+                                                <td>{}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
