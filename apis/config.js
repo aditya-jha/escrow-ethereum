150fe51f0d05fcc8c63737151822ce2dbbd344fb
Aditya Jha <aditya.jha@indifi.com>
2018-03-20 14:01:34 +0530

    added functionality to upload file

--- apis/config.js
+++ apis/config.js
@@ -4,8 +4,13 @@
 
 "use strict";
 
+const path = require("path");
+
 module.exports = {
     host: "127.0.0.1",
     port: "8082",
-    hostAddress: "http://127.0.0.1:8082"
+    hostAddress: "http://127.0.0.1:8082",
+    uploadDir: path.resolve(__dirname, "uploads"),
+    maxFileSize: "15mb",
+
 };
--- apis/routes/index.js
+++ apis/routes/index.js
@@ -5,10 +5,47 @@
 "use strict";
 
 const router = require("express").Router();
+const fs = require("fs");
+const crypto = require("crypto");
+
+const multer = require("multer")({
+    dest: global.Config.uploadDir,
+    limits: global.Config.maxFileSize
+});
 
 router.get("/", (req, res, next) => {
     res.send({
         data: "Hello World"
+    });
+});
+
+router.post("/upload-transactions-file", multer.single("file"), (req, res, next) => {
+    const uploadedFile = req.file;
+
+    let data = fs.readFileSync(uploadedFile.path).toString();
+    data = data.split("\n");
+
+    let parsed = [];
+    data.forEach(d => {
+        const dSplit = d.split("|");
+        parsed.push({
+            transactionId: dSplit[0],
+            escrowAccountNo: dSplit[1],
+            amount: parseFloat(dSplit[2]),
+            senderName: dSplit[3],
+            senderAccountNo: dSplit[4],
+            senderIFSC: dSplit[5],
+            date: dSplit[6],
+            nodalTransactionId: dSplit[7],
+            virtualAccountNo: `${dSplit[8]}${dSplit[9]}`,
+            mode: dSplit[10],
+            hash: crypto.createHash("sha256").update(d).digest("hex")
+        });
+    });
+
+    res.send({
+        success: true,
+        data: parsed
     });
 });
 
