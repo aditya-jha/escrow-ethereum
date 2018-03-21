81e276714e3a1324194a866d072518db20fd3db3
Aditya Jha <aditya.jha@indifi.com>
2018-03-21 19:20:21 +0530

    fix virtual account contract

--- contracts/EscrowTransactions.sol
+++ contracts/EscrowTransactions.sol
@@ -1,15 +1,14 @@
 pragma solidity ^0.4.18;
 
-
-import "./VirtualAccounts.sol";
 import "./IndifiCoin.sol";
 import "./SplitContract.sol";
+import "./VirtualAccounts.sol";
 
 
 contract EscrowTransactions is VirtualAccounts {
-    
+
     event NewTransaction(uint256 indexed transactionId, uint256 indexed amount);
-    
+
     struct Transaction {
         bytes32 transactionHash;
         uint256 amount;
@@ -17,119 +16,136 @@ contract EscrowTransactions is VirtualAccounts {
         uint timestamp;
     }
 
-    struct SplitShares {
-        uint256 borrowerShare;
-        uint256 lenderShare;
-    }
-
-    struct VirtualAccount {
-        bytes32 virtualAccountNumber;
-        address borrowerAddress;
-        address lenderAddress;
-        uint[] policyDetails;
-        string bankAccountNumber;
-        string ifscCode;
+    struct SplitTransaction {
+        uint256 shareAmount;
+        uint8 status;
     }
 
     Transaction[] public transactions;
-    
+    SplitTransaction[] public splitTransactions;
+
     IndifiCoin public indifiCoin;
     SplitContract public splitContract;
-    
+
     // -------------------------------------------------------------------------
     // To keep the position of any transaction in the transactions array
     // key: bytes32 - bytes32 representation of the sha256 hash of the transaction
     // value: uint256 - index of the Transaction struct in transactions array
     // -------------------------------------------------------------------------
     mapping(bytes32 => uint256) public transactionHashToIndex;
-    
-    // -------------------------------------------------------------------------
-    // To keep the status of the incoming transaction w.r.t settlement
-    // 0 - no such transaction
-    // 1 - transaction saved to blockchain
-    // 2 - split in coin contract done i.e. coins have been moved by indifi to lender and borrower respectively
-    // 3 - settlement request sent to bank
-    // 4 - settlement done
-    // -------------------------------------------------------------------------
-    mapping(uint256 => uint8) public transactionIdToStatus;
 
+    mapping(uint256 => uint256) public transactionIdToBorrowerSharesId;
 
-    // -------------------------------------------------------------------------
-    // To keep virtual account details againt virtual account number during onboarding
-    // key: bytes32 - bytes32 representation of the virtual account number
-    // value: VirtualAccount - virtual account details
-    // -------------------------------------------------------------------------
-    mapping(bytes32 => VirtualAccount) public VirtualAccounts;
-
-    mapping(uint256 => SplitShares) public transactionIdToSplitShares;
+    mapping(uint256 => uint256) public transactionIdToLenderSharesId;
 
     function EscrowTransactions() public {
         _createTransaction(0x0, 0, 0x0);
+        updateVirtualAccountConfiguration("", new uint[](2), address(0), address(0), "", "");
     }
-    
+
     function updateIndifiCoinAddress(address _indifiCoinAddress) public onlyOwner returns (bool) {
         indifiCoin = IndifiCoin(_indifiCoinAddress);
     }
-    
+
     function updateSplitContractAddress(address _splitContractAddress) public onlyOwner returns (bool) {
         splitContract = SplitContract(_splitContractAddress);
     }
-    
-    function newEscrowTransaction(string _hash, uint256 amount, string _vAccNo) public onlyOwner returns(bool) {
+
+    function newEscrowTransaction(string _hash, uint256 amount, string _vAccNo) public onlyOwner returns (bool) {
         // check if this transaction is new by comparing hash
         bytes32 hashBytes = _stringToBytes32(_hash);
         bytes32 vAccNoBytes = _stringToBytes32(_vAccNo);
-        
+
         require(transactionHashToIndex[hashBytes] <= 0);
-        
+
         // create new Transaction object and save
         uint256 id = _createTransaction(hashBytes, amount, vAccNoBytes);
-        
+
         // emit new transaction created
         NewTransaction(id, amount);
-        transactionIdToStatus[id] = 1; // transaction saved
 
         // allocate coins equal to the amount into owners account
         indifiCoin.createTokens(amount);
-        VirtualAccount memory va = VirtualAccounts[vAccNoBytes];
-        
+
+        // get VirtualAccount
+        VirtualAccount memory va = virtualAccounts[virtualAccountsToIndex[vAccNoBytes]];
+
         int256 lenderShare = splitContract.split(amount, va.policyDetails);
-        if (lenderShare != -1) {
+        if (lenderShare != - 1) {
+            uint256 borrowerShare = safeSub(amount, uint256(lenderShare));
             indifiCoin.transferTokens(va.lenderAddress, uint256(lenderShare));
-            indifiCoin.transferTokens(va.borrowerAddress, safeSub(amount, uint256(lenderShare)));
-            transactionIdToStatus[id] = 2; // split done
-            SplitShares memory ss =
+            indifiCoin.transferTokens(va.borrowerAddress, borrowerShare);
+
+            uint256 borrowerShareId = _createSplitTransaction(borrowerShare);
+            transactionIdToBorrowerSharesId[id] = borrowerShareId;
+
+            uint256 lenderShareId = _createSplitTransaction(uint256(lenderShare));
+            transactionIdToLenderSharesId[id] = lenderShareId;
         }
-        
+
         return true;
     }
-    
+
     function getTotalTransactions() public view returns (uint256) {
         return transactions.length - 1;
     }
 
-    function getTransaction(uint256 id) public view returns(
+    function getTransaction(uint256 id) public view returns (
         string transactionHash,
         uint256 amount,
-        string virtualAccountNumber
+        string virtualAccountNumber,
+        uint256 borrowerShareId,
+        uint256 lenderShareId
     ) {
         Transaction memory t = transactions[id];
         transactionHash = _bytes32ToString(t.transactionHash);
         amount = t.amount;
         virtualAccountNumber = _bytes32ToString(t.virtualAccountNumber);
+        borrowerShareId = transactionIdToBorrowerSharesId[id];
+        lenderShareId = transactionIdToLenderSharesId[id];
+    }
+
+    function updateSplitStatusToSentForSettlement(uint256[] ids) public returns (bool) {
+        for (uint256 i = 0; i < ids.length; i++) {
+            SplitTransaction memory st = splitTransactions[ids[i]];
+            if (st.status == 1) {
+                st.status = 2;
+                splitTransactions[ids[i]] = st;
+            }
+        }
+    }
+
+    function updateSplitStatusToSettled(uint256[] ids) public returns (bool) {
+        for (uint256 i = 0; i < ids.length; i++) {
+            SplitTransaction memory st = splitTransactions[ids[i]];
+            if (st.status == 2) {
+                st.status = 3;
+                splitTransactions[ids[i]] = st;
+            }
+        }
+    }
+
+    function _createSplitTransaction(uint256 _amount) internal returns (uint256) {
+        SplitTransaction memory st = SplitTransaction({
+            shareAmount : _amount,
+            status : 1
+            });
+
+        uint256 id = splitTransactions.push(st) - 1;
+        return id;
     }
 
     function _createTransaction(bytes32 _hashBytes, uint256 amount, bytes32 _vAccNoBytes) internal returns (uint256) {
         Transaction memory t = Transaction({
-            transactionHash: _hashBytes,
-            amount: amount,
-            virtualAccountNumber: _vAccNoBytes,
-            timestamp: now
+            transactionHash : _hashBytes,
+            amount : amount,
+            virtualAccountNumber : _vAccNoBytes,
+            timestamp : now
         });
-        
+
         uint256 id = transactions.push(t) - 1;
         transactionHashToIndex[_hashBytes] = id;
-        
+
         return id;
     }
 }
--- contracts/VirtualAccounts.sol
+++ contracts/VirtualAccounts.sol
@@ -1,52 +1,72 @@
 pragma solidity ^0.4.18;
+
 import "./AccessControl.sol";
 
-pragma experimental ABIEncoderV2;
 
 contract VirtualAccounts is AccessControl {
+
 
     struct VirtualAccount {
         bytes32 virtualAccountNumber;
         address borrowerAddress;
         address lenderAddress;
         uint[] policyDetails;
-        string bankAccountNumber;
-        string ifscCode;
+        bytes32 bankAccountNumber;
+        bytes32 ifscCode;
     }
 
+    // -------------------------------------------------------------------------
+    // To keep track of all virtual accounts created
+    // -------------------------------------------------------------------------
     VirtualAccount[] public virtualAccounts;
-    
+
     // -------------------------------------------------------------------------
     // To keep the position of any virtual account in the virtualaccounts array
     // key: bytes32 - bytes32 representation of the virtual account
-    // value: uint256 - index of the VirtualAccount struct in virtualaccounts array
+    // value: uint32 - index of the VirtualAccount struct in virtualaccounts array
     // -------------------------------------------------------------------------
     mapping(bytes32 => uint32) public virtualAccountsToIndex;
-    
-    
+
     function updateVirtualAccountConfiguration(
-        string _virtualAccountNumber, 
+        string _virtualAccountNumber,
         uint256[] _policyDetails,
         address _borrower,
         address _lender,
         string _bankAccountNumber,
         string _ifscCode
-    ) public onlyOwner returns (bool) {
+    ) public onlyOwner returns (uint32) {
         bytes32 virtualAccountNumberBytes = _stringToBytes32(_virtualAccountNumber);
         VirtualAccount memory va = VirtualAccount({
             virtualAccountNumber: virtualAccountNumberBytes,
             borrowerAddress: _borrower,
             lenderAddress: _lender,
             policyDetails: _policyDetails,
-            bankAccountNumber: _bankAccountNumber,
-            ifscCode: _ifscCode
-        });
+            bankAccountNumber: _stringToBytes32(_bankAccountNumber),
+            ifscCode: _stringToBytes32(_ifscCode)
+            });
         uint32 index = uint32(virtualAccounts.push(va) - 1);
         virtualAccountsToIndex[virtualAccountNumberBytes] = index;
+        return index;
     }
-    
-    function getAllVirtualAccounts() public view returns(VirtualAccount[]) {
-        return virtualAccounts;
+
+    function getTotalVirtualAccounts() public view returns (uint) {
+        return virtualAccounts.length - 1;
     }
-   
+
+    function getVirtualAccount(uint32 id) public view returns(
+        string virtualAccountNumber,
+        address borrowerAddress,
+        address lenderAddress,
+        uint[] policyDetails,
+        string bankAccountNumber,
+        string ifscCode
+    ) {
+        VirtualAccount memory va = virtualAccounts[id];
+        virtualAccountNumber = _bytes32ToString(va.virtualAccountNumber);
+        borrowerAddress = va.borrowerAddress;
+        lenderAddress = va.lenderAddress;
+        policyDetails = va.policyDetails;
+        bankAccountNumber = _bytes32ToString(va.bankAccountNumber);
+        ifscCode = _bytes32ToString(va.ifscCode);
+    }
 }
